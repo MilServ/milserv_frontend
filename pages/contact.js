fe7575957f8acf63cae673abe274f89b6ee12121
Juan Rivera <jrivera@primalplatoon.com>
2020-09-30 11:09:11 -0400

    Changed Contact form content to partner profile buttons.

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
-import ContactForm from "../components/form/ContactForm";
+import { SocialIcon } from "react-social-icons";
 
 const Contact = () => {
   return (
@@ -52,11 +52,11 @@ const Contact = () => {
                 style={{ borderRadius: 8 }}
               />
               <h2 className="program" style={{ color: "white" }}>
-                <b>Let's Stay Connected!</b>
+                <b>We appreciate your feedback!</b>
               </h2>
               <h5 className="program" style={{ color: "white" }}>
-                We appreciate your feedback and want to learn more about how we
-                can support your mission.
+                Please click on your partner profile below to be directed to the
+                appropriate contact form.
               </h5>
             </div>
           </div>
@@ -67,7 +67,66 @@ const Contact = () => {
           >
             <div className="col-md-8 mt-3 mb-2">
               {" "}
-              <ContactForm />
+              <div>
+                <ul id="horizontal-list">
+                  <li>
+                    <a href="/veteran-jobs">
+                      {" "}
+                      <button className="btn btn-warning m-1">Veterans</button>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="/retailers">
+                      <button className="btn btn-warning m-1">Retailers</button>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/distributors">
+                      <button className="btn btn-warning m-1">
+                        Distributors
+                      </button>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/makers">
+                      {" "}
+                      <button className="btn btn-warning m-1">Makers</button>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div
+            className="row text-center justify-content-center"
+            style={{ backgroundColor: "whitesmoke", paddingBottom: 10 }}
+          >
+            <div className="col-md-5 mt-3 mb-2">
+              <h2 className="program" style={{ color: "#383838" }}>
+                <b>Have a different reason for contacting us?</b>
+              </h2>
+            </div>
+          </div>
+          <div
+            className="row text-center justify-content-center"
+            style={{ backgroundColor: "whitesmoke", paddingBottom: 40 }}
+          >
+            <div className="col-md-6 mb-2">
+              <p>
+                <em>
+                  Drop us a message at any of the social links below or simply
+                  send an email to david.jones@acdmilserve.com.
+                </em>
+              </p>
+              <SocialIcon url="https://www.instagram.com/milserv/" />
+              <SocialIcon url="https://www.linkedin.com/company/american-craft-deliveries-milserve/" />
+              <img
+                src="../static/images/overlay.jpg"
+                alt="milserv.com picture"
+                width="100%"
+                style={{ marginTop: 10, borderRadius: "4px" }}
+              />
             </div>
           </div>
         </section>
